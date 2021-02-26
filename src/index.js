793242b9aedc35fa1a5d2eaa4ae8ac1cf171a12e
klltx <kostya.svetashov.01@gmail.com>
2021-02-26 23:12:21 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let res = 0;
+    if(n < 0) n = -n;
+    let str = n.toString();
+    for(let i = 0; i < str.length; i++){
+        res += Number(str[i]) * Math.pow(10, i);
+    }
+    console.log(res);
+    return res;
 }
